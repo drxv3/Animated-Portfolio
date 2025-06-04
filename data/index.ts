7a2e31706387a31e07ece1b234250fb4e2df89a9
Dhruv Kundu <120163455+drxv3@users.noreply.github.com>
2025-06-05 01:10:37 +0530

    Initial commit

---
+++ data/index.ts
@@ -0,0 +1,217 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Testimonials", link: "#testimonials" },
+    { name: "Contact", link: "#contact" },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "I value cross-functional teamwork enabled by open and effective communication",
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "/b1.svg",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: "I'm very flexible with time zone communications",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "My tech stack",
+      description: "I constantly try to improve",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "Cybersecurity enthusiast with a passion for Risk Management.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+  
+    {
+      id: 5,
+      title: "Currently building a JS Animation library",
+      description: "The Inside Scoop",
+      className: "md:col-span-3 md:row-span-2",
+      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+      titleClassName: "justify-center md:justify-start lg:justify-center",
+      img: "/b5.svg",
+      spareImg: "/grid.svg",
+    },
+    {
+      id: 6,
+      title: "Do you want to start a project together?",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 1,
+      title: "USB Rubber Ducky",
+      des: "Malicious USB device for reverse shell access on Windows using PowerShell and Netcat, enabling remote command execution and data exfiltration via a Linux listener.",
+      img: "/p1.jpg",
+      iconLists: ["/cpp.svg", "/powershell.svg", "/netcat.svg", "/linux.svg"],
+      link: "https://github.com/drxv3/USB-Rubber-Ducky",
+    },
+    {
+      id: 2,
+      title: "File Sharing Application",
+      des: "An Android-based file sharing app using Java and Firebase. Implements client-side encryption and unique decryption keys to ensure secure file transfers. Features user registration, intuitive UI, and real-time storage with Firebase integration.",
+      img: "/p2.png",
+      iconLists: ["/java.svg", "/xml.svg", "/firebase.svg", "/kotlin.svg", "/android_studio.svg"],
+      link: "https://github.com/drxv3/File-Sharing-Application",
+    },
+    {
+      id: 3,
+      title: "Skinister",
+      des: "Skinister is a web platform for CS:GO players to compare skin prices across multiple vendors. Built with HTML5, CSS3, and JavaScript, it features a responsive design, advanced search filters, and a user-friendly interface for efficient browsing.",
+      img: "/p3.png",
+      iconLists: ["/html5.svg", "/tail.svg", "/css.svg", "/js.svg"],
+      link: "https://github.com/adrianhajdin/ai_saas_app",
+    },
+    {
+      id: 4,
+      title: "Animated Apple Iphone 3D Website",
+      des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+      img: "/p4.svg",
+      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+      link: "https://github.com/adrianhajdin/iphone",
+    },
+  ];
+  
+  export const testimonials = [
+    {
+      quote:
+        "Dhruv has consistently demonstrated a level of technical acumen, discipline, and forward-thinking that is rare among professionals at his stage. His deep interest in cybersecurity and emerging technologies is matched by his ability to learn rapidly and execute with precision. What sets Dhruv apart is his initiative — he doesn't wait to be told what to do; he anticipates challenges, explores solutions, and delivers. It's evident he's not only building a career but preparing to lead in this domain.",
+      name: "Rakesh Kumar",
+      title: "Senior Assosiate at Kyndryl Technologies",
+    },
+    {
+      quote:
+        "On behalf of BigMid, I'd like to extend my deepest appreciation to Mr. Dhruv Kundu. Collaborating with someone of Dhruv's caliber in the field of data science has been both inspiring and enriching. His insightful understanding of data-driven strategies and his forward-thinking approach have brought exceptional value to our shared vision. At BigMid, we deeply value relationships built on innovation, trust, and mutual growth and Dhruv exemplifies all of these. We look forward to many more milestones together and are proud to have him as a vital part of our journey.",
+      name: "Siddharth Chaudhary",
+      title: "Co-Founder at BigMid Technologies",
+    },
+    {
+      quote:
+        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      name: "Satyam",
+      title: "Co-Founder at Xanthron",
+    },
+    {
+      quote:
+        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      name: "Mihhir Dhar",
+      title: "Founder and CEO at The Digital Doorstep",
+    },
+    {
+      quote:
+        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      name: "Michael Johnson",
+      title: "Director of AlphaStream Technologies",
+    },
+  ];
+  
+  export const companies = [
+    {
+      id: 1,
+      name: "cloudinary",
+      img: "/cloud.svg",
+      nameImg: "/cloudName.svg",
+    },
+    {
+      id: 2,
+      name: "appwrite",
+      img: "/app.svg",
+      nameImg: "/appName.svg",
+    },
+    {
+      id: 3,
+      name: "HOSTINGER",
+      img: "/host.svg",
+      nameImg: "/hostName.svg",
+    },
+    {
+      id: 4,
+      name: "stream",
+      img: "/s.svg",
+      nameImg: "/streamName.svg",
+    },
+    {
+      id: 5,
+      name: "docker.",
+      img: "/dock.svg",
+      nameImg: "/dockerName.svg",
+    },
+  ];
+  
+  export const workExperience = [
+    {
+      id: 1,
+      title: "Frontend Engineer Intern",
+      desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+      className: "md:col-span-2",
+      thumbnail: "/exp1.svg",
+    },
+    {
+      id: 2,
+      title: "Mobile App Dev - JSM Tech",
+      desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+      className: "md:col-span-2",
+      thumbnail: "/exp2.svg",
+    },
+    {
+      id: 3,
+      title: "Freelance App Dev Project",
+      desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+      className: "md:col-span-2",
+      thumbnail: "/exp3.svg",
+    },
+    {
+      id: 4,
+      title: "Lead Frontend Developer",
+      desc: "Developed and maintained user-facing features using modern frontend technologies.",
+      className: "md:col-span-2",
+      thumbnail: "/exp4.svg",
+    },
+  ];
+  
+  export const socialMedia = [
+    {
+      id: 1,
+      img: "/git.svg",
+    },
+    {
+      id: 2,
+      img: "/twit.svg",
+    },
+    {
+      id: 3,
+      img: "/link.svg",
+    },
+  ];
